7917609601bbeb0573b7a23a1cc2820f5c714715
David Godfrey <kinqvidda@gmail.com>
2025-10-27 23:40:25 +0100

    api service

--- app/src/main/java/com/example/retrofitpractice/ApiService.kt
+++ app/src/main/java/com/example/retrofitpractice/ApiService.kt
@@ -1,5 +1,8 @@
 package com.example.retrofitpractice
 
-interface ApiService {
+import retrofit2.http.GET
 
+interface ApiService {
+    @GET("posts")
+    suspend fun getPosts(): List<PostResponse>
 }
